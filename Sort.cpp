c1367329eddcdfe29224e58dc8b56fe20e62a8cb
mariessalogan <38187604+mariessalogan@users.noreply.github.com>
2019-01-13 15:04:00 -0800

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -66,6 +66,24 @@ void mergeSort(int *array, int left, int right)//This function is to halve the a
 	}
 }
 
+void insertSort(int *array, int length) //This function will sort using insertion.
+{
+	int i;
+	int current = 0; //This is going to be the value that we are sorting in the loop
+	int j = 0;
+	for (i = 0; i < length; i++)
+	{
+		current = array[i];
+		j = i - 1; //We want j to equal the previous number so we can compare
+		while (j >= 0 && current < arr[j])//We want to only run this if current is not the first number in the array and it is less than the prev number
+		{
+			array[j + 1] = array[j]; //Make the number after j, the current j, so you can move the larger numbers to the right
+			j--;
+		}
+		array[i] = current; //put the current iteration in that spot
+	}
+}
+
 int main()
 {
 	ifstream file; //The variable for the file to be read
@@ -79,21 +97,28 @@ int main()
 		cout << "Error, no file read\n";
 	}
 	else {
-		
+		std::cout << "The array from the file is:\n";
 		while (!file.eof())
 		{
 			file >> arr[n];
+			std::cout << arr[n] << "\n";
 			arr1[n] = arr[n];
 			arr2[n] = arr[n];
 			n++;
 		}
-		std::cout << "Your array has " << n << "numbers.\n";
+		std::cout << "Your array has " << n << " numbers.\n";
 		//merge sort
-		mergeSort(arr1, 0, n - 1);
+		mergeSort(arr1, 0, n - 1);//Calll the merge sort function on the arr1
 		std::cout << "Merge sort sorted your array to: \n";
 		for (int i = 0; i < n; i++)
 		{
 			cout << arr1[i] << "\n";
+		}
+		insertSort(arr2, n);//Call the insert sort function on arr2
+		std::cout << "Insert sort sorted your array to: \n";
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr2[i] << "\n";
 		}
 	}
 	file.close();
